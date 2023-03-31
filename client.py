812147955abae78da5023a66f5d27ab5970bd212
Trent Hardy <trentthomashardy123@gmail.com>
2023-03-31 17:56:11 -0500

    Incomplete ChatGPT code
    ChatGPT has a line limit apparently...

--- client.py
+++ client.py
@@ -1,41 +1,106 @@
 import socket
 import threading
+import select
+import time
+import sys
+import os
 
-# Server configuration
-host = 'localhost'
-port = 8000
+# Constants
+UDP_PORT = 10000
+TCP_PORT = 0 # port to be obtained from the server
+BUFFER_SIZE = 1024
+ACTIVITY_TIMEOUT = 300 # 5 minutes
 
-# Socket creation
-client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((host, port))
+# Client state
+client_id = "" # to be obtained from the user
+client_key = "" # to be obtained from the server
+cookie = "" # to be obtained from the server
+session_id = "" # to be obtained from the server
+connected = False
+server_address = "" # to be obtained from the user
+
+# Encryption function (XOR)
+def xor_crypt_string(data, key):
+    return ''.join(chr(ord(x) ^ ord(y)) for (x,y) in zip(data, key))
 
 # Function to receive messages from the server
-def receive():
-    while True:
-        try:
-            message = client_socket.recv(1024).decode()
-            print(message)
-        except:
-            # If there is an error, close the socket and exit the program
-            print("An error occurred!")
-            client_socket.close()
-            break
+def receive_message(sock):
+    while connected:
+        r, w, e = select.select([sock], [], [], 1)
+        if r:
+            data, addr = sock.recvfrom(BUFFER_SIZE)
+            if data:
+                # Decrypt the message
+                decrypted_data = xor_crypt_string(data, client_key)
+                # Handle the message
+                handle_message(decrypted_data)
+
+# Function to handle incoming messages
+def handle_message(data):
+    global session_id
+    global connected
+    split_data = data.split(" ")
+    message_type = split_data[0]
+    if message_type == "AUTH_FAIL":
+        print("Authentication failed")
+        connected = False
+    elif message_type == "AUTH_SUCCESS":
+        # Get the cookie and TCP port number
+        global cookie
+        global TCP_PORT
+        cookie = split_data[1]
+        TCP_PORT = int(split_data[2])
+        print("Connected to server")
+        connected = True
+    elif message_type == "CHAT_STARTED":
+        # Get the session ID
+        session_id = split_data[1]
+        print("Chat started with " + split_data[2])
+    elif message_type == "UNREACHABLE":
+        print("The requested client is not reachable")
+    elif message_type == "END_NOTIF":
+        print("Chat ended")
+        session_id = ""
+    else:
+        # Unknown message type
+        print("Unknown message type")
 
 # Function to send messages to the server
-def send():
-    while True:
-        message = input()
-        client_socket.send(message.encode())
+def send_message(sock, message):
+    # Encrypt the message
+    encrypted_message = xor_crypt_string(message, client_key)
+    # Send the message
+    sock.sendto(encrypted_message, (server_address, UDP_PORT))
 
-# Prompt user for their username
-username = input("Please enter your username: ")
+# Function to send chat messages to the server
+def send_chat_message(sock, message):
+    # Encrypt the message
+    encrypted_message = xor_crypt_string(message, client_key)
+    # Send the message
+    sock.send(encrypted_message)
 
-# Send username to server
-client_socket.send(username.encode())
-
-# Start threads for sending and receiving messages
-receive_thread = threading.Thread(target=receive)
-receive_thread.start()
-
-send_thread = threading.Thread(target=send)
-send_thread.start()
+# Function to handle chat sessions
+def chat_session(sock):
+    global session_id
+    if session_id == "":
+        print("You are not currently in a chat session")
+        return
+    while connected:
+        r, w, e = select.select([sock, sys.stdin], [], [], 1)
+        if r:
+            if sock in r:
+                # Receive message from server and decrypt it
+                data = sock.recv(BUFFER_SIZE)
+                decrypted_data = xor_crypt_string(data, client_key)
+                # Print the message
+                print(decrypted_data)
+            else:
+                # Get user input and send chat message to server
+                message = sys.stdin.readline().strip()
+                if message == "End Chat":
+                    # End the chat session
+                    send_message(sock, "END_REQUEST " + session_id)
+                    session_id = ""
+                    break
+                else:
+                    # Send
--- server.py
+++ server.py
@@ -1,62 +1,81 @@
 import socket
 import threading
 
-# Server configuration
-host = 'localhost'
-port = 8000
+# list of subscribers with their client IDs and secret keys
+subscribers = {
+    'ClientA': 'SecretKeyA',
+    'ClientB': 'SecretKeyB',
+    'ClientC': 'SecretKeyC'
+}
 
-# Socket creation
-server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-server_socket.bind((host, port))
-server_socket.listen()
+# dictionary to keep track of connected clients and their keys
+connected_clients = {}
 
-# Clients and their usernames
-clients = {}
-usernames = []
+# dictionary to keep track of ongoing chat sessions
+chat_sessions = {}
 
-# Function to broadcast messages to all connected clients
-def broadcast(message):
-    for client_socket in clients:
-        client_socket.send(message)
+# UDP port number for authentication and challenge-response protocol
+UDP_PORT = 5000
 
-# Function to handle each client's connection
-def handle_client(client_socket, username):
-    # Welcome message to new client
-    client_socket.send(f"Welcome {username}! You are now connected to the chat room.\n".encode())
+# TCP port number for chat sessions
+TCP_PORT = 6000
 
-    # Notify all other clients that a new client has joined
-    broadcast(f"{username} has joined the chat room!\n".encode())
 
+def handle_udp_auth(connection, address):
+    """Handle the authentication and challenge-response protocol for a client"""
+    data, client_address = connection.recvfrom(1024)
+    client_id = data.decode('utf-8').split(' ')[1]
+    if client_id in subscribers:
+        secret_key = subscribers[client_id]
+        # generate random challenge
+        rand = 'random_challenge'
+        connection.sendto(rand.encode('utf-8'), client_address)
+        data, client_address = connection.recvfrom(1024)
+        response = data.decode('utf-8').split(' ')[1]
+        if response == secret_key:
+            # generate encryption key
+            ck = 'encryption_key'
+            connected_clients[client_id] = {'address': client_address, 'key': ck}
+            # send authentication success message
+            auth_success_message = f'AUTH_SUCCESS({rand}, {TCP_PORT})'
+            encrypted_message = ck + auth_success_message
+            connection.sendto(encrypted_message.encode('utf-8'), client_address)
+        else:
+            # send authentication failure message
+            connection.sendto('AUTH_FAIL'.encode('utf-8'), client_address)
+
+
+def handle_tcp_chat(connection, address):
+    """Handle chat sessions between clients"""
+    # receive initial CONNECT message with random cookie
+    data = connection.recv(1024).decode('utf-8')
+    rand_cookie = data.split(' ')[1]
+    client_id = ''
+    for c_id, client in connected_clients.items():
+        if client['address'] == address:
+            client_id = c_id
+            break
     while True:
-        # Receive message from client
-        message = client_socket.recv(1024)
-
-        # Broadcast message to all other clients
-        broadcast(f"{username}: {message.decode()}".encode())
-
-    # Close client socket when they disconnect
-    client_socket.close()
-
-# Function to start the server
-def start_server():
-    print(f"Server listening on {host}:{port}...\n")
-
-    while True:
-        # Accept client connections
-        client_socket, client_address = server_socket.accept()
-
-        # Prompt client for their username
-        client_socket.send("Please enter your username: ".encode())
-        username = client_socket.recv(1024).decode().strip()
-
-        # Add client to list of connected clients
-        clients[client_socket] = username
-        usernames.append(username)
-
-        # Handle each client's connection in a new thread
-        thread = threading.Thread(target=handle_client, args=(client_socket, username))
-        thread.start()
-
-# Start the server
-start_server()
+        data = connection.recv(1024).decode('utf-8')
+        if data.startswith('CHAT_REQUEST'):
+            # initiate chat session with another client
+            target_id = data.split(' ')[1]
+            if target_id in connected_clients and target_id != client_id:
+                session_id = 'random_session_id'
+                chat_sessions[session_id] = (client_id, target_id)
+                message_a = f'CHAT_STARTED({session_id}, {target_id})'
+                message_b = f'CHAT_STARTED({session_id}, {client_id})'
+                target_address = connected_clients[target_id]['address']
+                send_tcp_message(message_a, target_address, connected_clients[target_id]['key'])
+                send_tcp_message(message_b, address, connected_clients[client_id]['key'])
+            else:
+                message = f'UNREACHABLE({target_id})'
+                send_tcp_message(message, address, connected_clients[client_id]['key'])
+        elif data.startswith('END_REQUEST'):
+            # end chat session
+            session_id = data.split(' ')[1]
+            if session_id in chat_sessions and client_id in chat_sessions[session_id]:
+                other_id = chat_sessions[session_id][0] if client_id == chat_sessions[session_id][1] else chat_sessions[session_id][1]
+                message = f'END_NOTIF({session_id})'
+                other_address = connected_clients[other_id]['address']
+                send_tcp_message(message

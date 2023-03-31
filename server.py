84525904a12ad0b81114cd552d7558fbce17e762
Trent Hardy <trentthomashardy123@gmail.com>
2023-03-31 18:17:33 -0500

    working server.py
    AI pulled through for once?

--- server.py
+++ server.py
@@ -1,81 +1,95 @@
 import socket
 import threading
+import random
+import string
 
-# list of subscribers with their client IDs and secret keys
+# List of subscribers with their Client IDs and secret keys
 subscribers = {
-    'ClientA': 'SecretKeyA',
-    'ClientB': 'SecretKeyB',
-    'ClientC': 'SecretKeyC'
+    "Alice": "p@ssw0rd1",
+    "Bob": "p@ssw0rd2",
+    "Charlie": "p@ssw0rd3"
 }
 
-# dictionary to keep track of connected clients and their keys
+# List of connected clients with their Client IDs and encryption keys
 connected_clients = {}
 
-# dictionary to keep track of ongoing chat sessions
-chat_sessions = {}
-
-# UDP port number for authentication and challenge-response protocol
-UDP_PORT = 5000
-
-# TCP port number for chat sessions
-TCP_PORT = 6000
+# List of active chat sessions with their session IDs and client IDs
+active_chat_sessions = {}
 
 
-def handle_udp_auth(connection, address):
-    """Handle the authentication and challenge-response protocol for a client"""
-    data, client_address = connection.recvfrom(1024)
-    client_id = data.decode('utf-8').split(' ')[1]
+def generate_random_string(length):
+    """Helper function to generate a random string of given length"""
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
+
+
+def handle_udp_connection(sock, addr):
+    """Thread function to handle UDP connections"""
+    data, _ = sock.recvfrom(1024)
+    message = data.decode()
+    client_id = message.split()[1]
+
+    # Verify that the client ID is a subscriber
     if client_id in subscribers:
         secret_key = subscribers[client_id]
-        # generate random challenge
-        rand = 'random_challenge'
-        connection.sendto(rand.encode('utf-8'), client_address)
-        data, client_address = connection.recvfrom(1024)
-        response = data.decode('utf-8').split(' ')[1]
-        if response == secret_key:
-            # generate encryption key
-            ck = 'encryption_key'
-            connected_clients[client_id] = {'address': client_address, 'key': ck}
-            # send authentication success message
-            auth_success_message = f'AUTH_SUCCESS({rand}, {TCP_PORT})'
-            encrypted_message = ck + auth_success_message
-            connection.sendto(encrypted_message.encode('utf-8'), client_address)
+        random_number = generate_random_string(8).encode()
+        response = f"CHALLENGE {random_number.decode()}"
+        sock.sendto(response.encode(), addr)
+        data, _ = sock.recvfrom(1024)
+        response = data.decode()
+        expected_response = f"RESPONSE {secret_key} {random_number.decode()}"
+        if response == expected_response:
+            encryption_key = generate_random_string(16)
+            connected_clients[client_id] = encryption_key
+            rand_cookie = generate_random_string(8)
+            port_number = random.randint(5000, 65535)
+            response = f"AUTH_SUCCESS {rand_cookie} {port_number}"
+            encrypted_response = response.encode("utf-8")
+            cipher = encryption_key.encode("utf-8")
+            encrypted_data = bytearray([(encrypted_response[i] ^ cipher[i % len(cipher)]) for i in range(len(encrypted_response))])
+            sock.sendto(encrypted_data, addr)
+            print(f"{client_id} authenticated successfully")
         else:
-            # send authentication failure message
-            connection.sendto('AUTH_FAIL'.encode('utf-8'), client_address)
+            response = "AUTH_FAIL"
+            sock.sendto(response.encode(), addr)
+            print(f"{client_id} failed to authenticate")
+    else:
+        response = "AUTH_FAIL"
+        sock.sendto(response.encode(), addr)
+        print(f"{client_id} failed to authenticate")
 
 
-def handle_tcp_chat(connection, address):
-    """Handle chat sessions between clients"""
-    # receive initial CONNECT message with random cookie
-    data = connection.recv(1024).decode('utf-8')
-    rand_cookie = data.split(' ')[1]
-    client_id = ''
-    for c_id, client in connected_clients.items():
-        if client['address'] == address:
-            client_id = c_id
-            break
-    while True:
-        data = connection.recv(1024).decode('utf-8')
-        if data.startswith('CHAT_REQUEST'):
-            # initiate chat session with another client
-            target_id = data.split(' ')[1]
-            if target_id in connected_clients and target_id != client_id:
-                session_id = 'random_session_id'
-                chat_sessions[session_id] = (client_id, target_id)
-                message_a = f'CHAT_STARTED({session_id}, {target_id})'
-                message_b = f'CHAT_STARTED({session_id}, {client_id})'
-                target_address = connected_clients[target_id]['address']
-                send_tcp_message(message_a, target_address, connected_clients[target_id]['key'])
-                send_tcp_message(message_b, address, connected_clients[client_id]['key'])
-            else:
-                message = f'UNREACHABLE({target_id})'
-                send_tcp_message(message, address, connected_clients[client_id]['key'])
-        elif data.startswith('END_REQUEST'):
-            # end chat session
-            session_id = data.split(' ')[1]
-            if session_id in chat_sessions and client_id in chat_sessions[session_id]:
-                other_id = chat_sessions[session_id][0] if client_id == chat_sessions[session_id][1] else chat_sessions[session_id][1]
-                message = f'END_NOTIF({session_id})'
-                other_address = connected_clients[other_id]['address']
-                send_tcp_message(message
+def handle_tcp_connection(sock, addr):
+    """Thread function to handle TCP connections"""
+    data = sock.recv(1024)
+    message = data.decode()
+    if message.startswith("CONNECT"):
+        rand_cookie = message.split()[1]
+        response = "CONNECTED"
+        sock.send(response.encode())
+        client_id = None
+        for cid, ck in connected_clients.items():
+            if ck == connected_clients.get(rand_cookie):
+                client_id = cid
+                break
+        if client_id is None:
+            print("Error: client ID not found")
+            return
+        active_chat_sessions[rand_cookie] = (client_id, sock)
+        print(f"Chat session {rand_cookie} started")
+
+
+def handle_chat_request(sock, addr, session_id, client_id_b):
+    """Thread function to handle chat requests"""
+    if client_id_b not in connected_clients:
+        response = f"UNREACHABLE {client_id_b}"
+        sock.send(response.encode())
+        return
+    for sid, (cid_a, sock_a) in active_chat_sessions.items():
+        if cid_a == client_id_b:
+            response = f"BUSY {client_id_b}"
+            sock.send(response.encode())
+            return
+    cid_a = None
+    for cid, ck in connected_clients.items():
+        if ck == connected_clients.get(session_id):
+            cid_a = cid
